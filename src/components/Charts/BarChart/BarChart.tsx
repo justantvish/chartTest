699b41a3cc1453a830d4da0ecb7f66b02d3394ad
justantvish <justantvish@gmail.com>
2024-06-20 14:47:06 +0300

    step-3: annotations data point, tooltip customization

--- src/components/Charts/BarChart/BarChart.tsx
+++ src/components/Charts/BarChart/BarChart.tsx
@@ -1,79 +1,140 @@
+import { renderToStaticMarkup } from "react-dom/server";
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
+
 import { API_URL } from "../../../constants";
+import { currencyItemObj } from "../../../interfaces";
+
+import { fetchResponse, useFetch } from "../../../hooks/useFetch";
+import Tooltip from "../../UI/Tooltip/Tooltip";
 
 import classes from './BarChart.module.scss';
-import { fetchResponse, useFetch } from "../../../hooks/useFetch";
-import { currencyItemObj } from "../../../interfaces";
 
 const BarChart = () => {
     const {data}: fetchResponse = useFetch(API_URL);
     console.log(data)
 
-const categories: string[] = [];
-const marketCapData: number[] = [];
-const marketCapChangeData: number[] = [];
-    
-data && data.map((cur: currencyItemObj) => {
-    categories.push(cur.name)
-    marketCapData.push(cur.market_cap)
-    marketCapChangeData.push(cur.market_cap_change_24h)
-})
+    const categories: string[] = [];
+    const marketCapData: number[] = [];
+    const marketCapChangeData: number[] = [];
+        
+    data && data.map((cur: currencyItemObj) => {
+        categories.push(cur.name)
+        marketCapData.push(Number((cur.market_cap / 1000000000).toFixed(2)))
+        marketCapChangeData.push(Number((cur.market_cap_change_24h / 1000000000).toFixed(2)))
+    });
 
-console.log(marketCapChangeData.map(el => el.toLocaleString()))
+    const findMaxCap = () => {
+        let highestMarketCap: number = 0;
+        let highestMarketCapCurrency: string = '';
 
-const chartOptions: ApexOptions = {
-    colors: ['#fabfbf', '#797878'],
-    chart: {
-        type: 'bar',
-        height: 350
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: '80%',
-        },
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent']
-      },
-      xaxis: {
-        categories: [...categories],
-      },
-      yaxis: {
-        title: {
-          text: 'Market Cap ($)',
+        for (let i = 0; i < data.length; i++) {
+            const current: currencyItemObj = data[i];
+            highestMarketCap = Math.max(highestMarketCap, current.market_cap)
+            if(current.market_cap === highestMarketCap) {
+                highestMarketCapCurrency = current.name
+            }
         }
-      },
-      fill: {
-        opacity: 1
-      },
-      tooltip: {
-        enabled: true,
-        intersect: false,
-        shared: true,
-        y: {
-          formatter: function (val) {
-            return `$${val}`
-          }
-        }
+        highestMarketCap = Number((highestMarketCap / 1000000000).toFixed(2))
+        return {highestMarketCap, highestMarketCapCurrency};
     }
-}
+    const {highestMarketCap, highestMarketCapCurrency} = findMaxCap();
+   
 
-const series = [
-    {
-        name: 'Market Cap',
-        data: [...marketCapData],
-    }, {
-        name: 'Market Cap change (24h)',
-        data: [...marketCapChangeData],
-    },
-];
+
+    console.log(findMaxCap())
+
+    const chartOptions: ApexOptions = {
+        annotations: {
+            points: [
+                {
+                    x: highestMarketCapCurrency,
+                    y: highestMarketCap,
+                    seriesIndex: 0,
+                    marker: {
+                        size: 6,
+                        fillColor: "#fabfbf",
+                        strokeColor: "#363232",
+                        radius: 2
+                    },
+                    label: {
+                        borderColor: "#fabfbf",
+                        offsetY: 0,
+                        style: {
+                            color: "#fff",
+                            background: "#fabfbf",
+                            fontSize: '12'
+                        },
+                        text: 'Highest Market Cap'
+                    }
+                }
+            ]
+        },
+        colors: ['#fabfbf', '#797878'],
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+            horizontal: false,
+            columnWidth: '80%',
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: [...categories],
+        },
+        yaxis: {
+            title: {
+            text: 'Market Cap (in billion USD)',
+            }
+        },
+        fill: {
+            opacity: 1
+        },
+        tooltip: {
+            custom: function({series, seriesIndex, dataPointIndex, w}) {
+                const curObj = data.find(item => item['name'] === w.globals.labels[dataPointIndex]);
+
+                if (curObj === undefined) {
+                    throw new TypeError('The value was promised to always be there!');
+                }
+                const imgUrl = curObj['image'];
+
+                return renderToStaticMarkup (
+                    <Tooltip imgUrl={imgUrl} title={w.globals.labels[dataPointIndex]}>
+                        Market Cap: {series[seriesIndex][dataPointIndex]}
+                    </Tooltip>
+                );
+            },
+            intersect: false,
+            theme: undefined,
+            shared: true,
+            y: {
+                formatter: function (val) {
+                    return `$${val}`
+                }
+            }
+        },
+    }
+
+    const series = [
+        {
+            name: 'Market Cap',
+            data: [...marketCapData],
+        }, {
+            name: 'Market Cap change (24h)',
+            data: [...marketCapChangeData],
+        },
+    ];
 
     return (
         <div id="chart" className={classes.chart}>
